ecb3b068ad4e2fc182af8bb01a3ff135455cbb4e
Brenna McLeod <brenna.mcleod94@gmail.com>
2021-04-25 12:19:54 -0500

    required db.js in index.html

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,79 +1,74 @@
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/index.js",
-    "/db.js",
-    "/styles.css",
-    "/manifest.webmanifest",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png",
-  ];
-  
-  const STATIC_CACHE = "static-cache";
-  const DATA_CACHE = "data-cache";
-  
-  // install - adds all of the files specified above to the cache so it can work offline
-  self.addEventListener("install", function(evt) {
-    evt.waitUntil(
-      caches.open(STATIC_CACHE).then(cache => {
-        console.log("Your files were pre-cached successfully!");
-        return cache.addAll(FILES_TO_CACHE);
-      })
-    );
-  
-    self.skipWaiting();
-  });
-  
+  "/",
+  "./index.html",
+  "./index.js",
+  "./styles.css",
+  "./manifest.webmanifest",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png",
+  "./db.js"
+];
 
-// loops through available caches and deletes any old caches
-  self.addEventListener("activate", function(evt) {
-    evt.waitUntil(
-      caches.keys().then(keyList => {
-        return Promise.all(
-          keyList.map(key => {
-            if (key !== STATIC_CACHE && key !== DATA_CACHE) {
-              console.log("Removing old cache data", key);
-              return caches.delete(key);
-            }
-          })
-        );
-      })
-    );
-  
-    self.clients.claim();
-  });
-  
-  // fetch
-  self.addEventListener("fetch", function(evt) {
-    // cache successful requests to the API
-    if (evt.request.url.includes("/api/")) {
-      evt.respondWith(
-        caches.open(DATA_CACHE).then(cache => {
-          return fetch(evt.request)
-            .then(response => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-  
-              return response;
-            })
-            .catch(err => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
-        }).catch(err => console.log(err))
+const STATIC_CACHE = "static-cache-v1";
+const DATA_CACHE = "data-cache-v1";
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(STATIC_CACHE).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== STATIC_CACHE && key !== DATA_CACHE) {
+            return caches.delete(key);
+          }
+        })
       );
-  
-      return;
-    }
-  
-    // if the request is not for the API, serve static assets using "offline-first" approach.
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (evt) => {
+  if (evt.request.url.includes("/api/")) {
     evt.respondWith(
-      caches.match(evt.request).then(function(response) {
-        return response || fetch(evt.request);
-      })
+      caches.open(DATA_CACHE).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
     );
-  });
-  
-  
+
+    return;
+  }
+
+  evt.respondWith(
+    fetch(evt.request).catch(() => {
+      return caches.match(evt.request).then((response) => {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
